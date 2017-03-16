0549aa3a76388eecbcee1eaa6f70ba6fe34f2b95
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-03-16 09:03:17 -0400

    Use frequency to minimize chance of StackOverflowError.

--- src/test/scala/net/MySpec.scala
+++ src/test/scala/net/MySpec.scala
@@ -1,18 +1,25 @@
 package net
 
-import org.scalacheck._
-import Gen.{oneOf, const, lzy}
-
-// abstract class MyList[+A]
-// case class Cons[+A](elem: A, rest: MyList[A]) extends MyList[A]
-// case object Empty                             extends MyList[Nothing]
+import org.scalacheck.Gen
+import Gen.{oneOf, const, lzy, frequency}
 
 object MySpec {
 
 	def genList[A](gen: Gen[A]): Gen[MyList[A]] =
-		lzy { oneOf(genCons(gen), Gen.const(Empty)) } 
+		lzy { genListHelper[A](gen) } 
 
-	def genCons[A](gen: Gen[A]): Gen[MyList[A]] = for {
+	private val genEmpty: Gen[MyList[Nothing]] = Gen.const(Empty)
+
+	// http://stackoverflow.com/questions/42810351/testing-recursive-data-structure/42816037#42816037
+	private def genListHelper[A](gen: Gen[A]): Gen[MyList[A]] = 
+		lzy { 
+			frequency[MyList[A]](
+				(10, genCons(gen)),
+				(1, genEmpty     )
+			)
+		}
+
+	private def genCons[A](gen: Gen[A]): Gen[MyList[A]] = for {
 		list <- genList(gen)
 		a    <- gen
 	} yield Cons(a, list)
